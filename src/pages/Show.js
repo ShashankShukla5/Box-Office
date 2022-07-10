944c2e027396b3f7198c994b852976f9db90400c
shanky <shashank9450158183@gmail.com>
2022-07-10 14:24:00 +0530

    created two new more hooks: useLastQuery and useShow

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -4,53 +4,12 @@ import Cast from "../components/show/Cast";
 import Details from "../components/show/Details";
 import Seasons from "../components/show/Seasons";
 import ShowMainData from "../components/show/ShowMainData";
-import { apiGet } from "../misc/config";
+import { useShows } from "../misc/Custom-hooks";
 import { InfoBlock, ShowPageWrapper } from "./Show.styled";
-
-const reducer = (prevState, action) => {
-  switch (action.type) {
-    case "FETCH_SUCCESS": {
-      return { isLoading: false, error: null, show: action.show };
-    }
-
-    case "FETCH_FAILED": {
-      return { ...prevState, isLoading: false, error: action.error };
-    }
-
-    default:
-      return prevState;
-  }
-};
-
-const initialState = {
-  show: null,
-  isLoading: true,
-  error: null,
-};
-
 const Show = () => {
   const { id } = useParams();
 
-  const [{ show, isLoading, error }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
-
-  // const [show, setShow] = useState(null);
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [error, setError] = useState(null);
-
-  useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
-      .then((results) => {
-        dispatch({ type: "FETCH_SUCCESS", show: results });
-      })
-      .catch((err) => {
-        dispatch({ type: "FETCH_FAILED", error: err.message });
-      });
-  }, [id]);
-
-  console.log("show", show);
+  const { show, isLoading, error } = useShows(id);
 
   if (isLoading) {
     return <div>Data is being loaded</div>;
