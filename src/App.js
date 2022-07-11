cc2081984a9f3170d431b4f9dc2688fa7707c3bc
shanky <shashank9450158183@gmail.com>
2022-07-11 23:36:19 +0530

    defined theme for styled components and created a few styled components

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import Navs from "./components/Navs";
 import Home from "./pages/Home";
 import Starred from "./pages/Starred";
 import Show from "./pages/Show";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  mainColors: {
+    blue: "#2400ff",
+    gray: "#c6c6c6",
+    dark: "#353535",
+  },
+};
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/starred" element={<Starred />} />
-        <Route exact path="/show/:id" element={<Show />} />
-        <Route element={<div>Not Found</div>} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/starred" element={<Starred />} />
+          <Route exact path="/show/:id" element={<Show />} />
+          <Route element={<div>Not Found</div>} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
